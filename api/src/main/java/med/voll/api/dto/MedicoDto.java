2785f899cdf2e4ec5cadf2c62afedc67903b9440
uluizeduardo <luiz.analistadesistemas@gmail.com>
2023-03-11 22:27:48 -0300

    Refatorado MedicoDto

--- api/src/main/java/med/voll/api/dto/MedicoDto.java
+++ api/src/main/java/med/voll/api/dto/MedicoDto.java
@@ -2,5 +2,5 @@ package med.voll.api.dto;
 
 import med.voll.api.enums.Especialidade;
 
-public record MedicoDto(String nome, String email, String crm, Especialidade especialidade, EnderecoDto enderecoDto) {
+public record MedicoDto(String nome, String email, String crm, Especialidade especialidade, EnderecoDto endereco) {
 }
