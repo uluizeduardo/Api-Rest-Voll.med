f967a4bf74dfe2bc1d2bd259cb9c523fdd839061
uluizeduardo <luiz.analistadesistemas@gmail.com>
2023-03-26 00:05:50 -0300

    Adicionado o campo id ao dto ListaDeMedicosDto, pois precisaremos dele no endpoit de atualização de registros

--- api/src/main/java/med/voll/api/dto/ListaDeMedicosDto.java
+++ api/src/main/java/med/voll/api/dto/ListaDeMedicosDto.java
@@ -3,9 +3,9 @@ package med.voll.api.dto;
 import med.voll.api.enums.Especialidade;
 import med.voll.api.model.Medico;
 
-public record ListaDeMedicosDto(String nome, String email, String crm, Especialidade especialidade) {
+public record ListaDeMedicosDto(Long id, String nome, String email, String crm, Especialidade especialidade) {
 
     public ListaDeMedicosDto(Medico medico){
-      this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+      this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
     }
 }
